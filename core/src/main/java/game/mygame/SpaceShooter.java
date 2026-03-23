d67163736c512e64a00134c61eb3bfa47dc13da6
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-03-23 19:31:32 +0500

    Initial project setup
    Set up libGDX project skeleton. Entry point class SpaceShooter extends Game.
    SpriteBatch initialized will be shared across screens to avoid redundant allocations.
    Screens and game logic to follow.

--- core/src/main/java/game/mygame/SpaceShooter.java
+++ core/src/main/java/game/mygame/SpaceShooter.java
@@ -1,4 +1,18 @@
 package game.mygame;
 
-public class SpaceShooter {
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SpaceShooter extends Game {
+    public SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        // TODO: add screens
+    }
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
