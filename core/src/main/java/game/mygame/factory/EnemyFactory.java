ef7320d658a187c868d4bce6a29404cb447aee4c
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-03-24 01:58:28 +0500

    Implement EnemyFactory (Factory pattern)
    Three enemy types: SLOW (120 spd, 2 HP, 100pts), FAST (220 spd, 1 HP, 50pts),
    TANK (60 spd, 5 HP, 200pts).
    
    Factory pattern chosen because:
    - Construction details (speed, hp, score) should not leak into GameScreen
    - Adding new enemy types in the future requires changes in one place only
    - Caller simply does: enemyFactory.create(EnemyType.FAST, x, y)

--- core/src/main/java/game/mygame/factory/EnemyFactory.java
+++ core/src/main/java/game/mygame/factory/EnemyFactory.java
@@ -1,4 +1,34 @@
 package game.mygame.factory;
 
+import com.badlogic.gdx.graphics.Texture;
+import game.mygame.entities.Enemy;
+
 public class EnemyFactory {
+    public enum EnemyType {
+        SLOW,
+        FAST,
+        TANK
+    }
+
+    private final Texture slowTexture;
+    private final Texture fastTexture;
+    private final Texture tankTexture;
+
+    public EnemyFactory(Texture slowTexture, Texture fastTexture, Texture tankTexture) {
+        this.slowTexture = slowTexture;
+        this.fastTexture = fastTexture;
+        this.tankTexture = tankTexture;
+    }
+
+    public Enemy create(EnemyType type, float x, float y) {
+        switch (type) {
+            case FAST:
+                return new Enemy(x, y, 220f, 1, 50,  fastTexture);
+            case TANK:
+                return new Enemy(x, y,  60f, 5, 200, tankTexture);
+            case SLOW:
+            default:
+                return new Enemy(x, y, 120f, 2, 100, slowTexture);
+        }
+    }
 }
