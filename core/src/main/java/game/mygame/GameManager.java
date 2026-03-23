257b756437077b344034d6f434ee64bc1e72bf28
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-03-23 20:08:34 +0500

    Add GameManager using Singleton pattern
    Score, lives and game-over state need to be accessible from multiple
    places (screens, entities) without passing references around.
    Singleton guarantees one shared instance via getInstance().
    Private constructor prevents accidental second instantiation.
    Observer support will be added once enemies are in place.

--- core/src/main/java/game/mygame/GameManager.java
+++ core/src/main/java/game/mygame/GameManager.java
@@ -1,4 +1,39 @@
 package game.mygame;
 
 public class GameManager {
+    private static GameManager instance;
+
+    private int score;
+    private int lives;
+    private boolean gameOver;
+
+    private GameManager() {}
+
+    public static GameManager getInstance() {
+        if (instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    public void init() {
+        score = 0;
+        lives = 3;
+        gameOver = false;
+    }
+
+    public void addScore(int points) {
+        score += points;
+    }
+
+    public void loseLife() {
+        lives--;
+        if (lives <= 0) {
+            gameOver = true;
+        }
+    }
+
+    public int getScore()       { return score; }
+    public int getLives()       { return lives; }
+    public boolean isGameOver() { return gameOver; }
 }
